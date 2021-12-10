a3411cfb3c14e7be8dae0788acc11565976f6802
Lam <LamDuong.inbox@gmail.com>
2021-12-09 19:42:34 -0800

    commit tree 8aaaa8f5f2f2ee5725e170ee49185dc8bc9fc910

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actItem = actors.map(act => (
+    <div key={act.name}>
+      <h2>{act.name}</h2>
+      <ul>
+        {act.movies.map(movie => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actItem}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const dirItem = directors.map(dir => (
+    <div key={dir.name}>
+      <h2>{dir.name}</h2>
+      <ul>
+        {dir.movies.map(movie => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {dirItem}
+    </div>
+  )
 }
 
 export default Directors;
